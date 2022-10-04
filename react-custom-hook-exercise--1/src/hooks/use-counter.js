8ae1d253165fbf7292b0c85136e8980cf1b4fb96
Marving Moreton <moreton.marving@gmail.com>
2022-10-03 21:08:06 -0400

    initial commit

---
+++ react-custom-hook-exercise--1/src/hooks/use-counter.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+const useCounter = (forwards = true) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [forwards]);
+
+  return counter;
+};
+
+// The parameter (forwards) is a dependency! It's not defined inside of the useEffect(() function
+// neither in the useCounter function; so we need to FWD it as a dependency in the useEffect()
+
+export default useCounter;
