5fd25e212d056abe977fe7a4f2678ac587b0b88e
Marving Moreton <moreton.marving@gmail.com>
2022-10-06 12:23:13 -0400

    rollback- stopped @ SSR discover

--- 06-onwards-to-a-bigger-project-with-next-js/.next/static/chunks/webpack.js
+++ 06-onwards-to-a-bigger-project-with-next-js/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "03ab4e9b81acedbb4269";
+/******/ 	var hotCurrentHash = "83aeec7662e675d0be4e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
